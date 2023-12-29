56322c6ab269c2ac8292b65cf93baa2871296a98
김수환 <101265039+genius00hwan@users.noreply.github.com>
2023-12-29 20:27:39 +0900

    initial commit

---
+++ src/main/java/jpa_book_project/soohwan/SoohwanApplication.java
@@ -0,0 +1,13 @@
+package jpa_book_project.soohwan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoohwanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoohwanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa_book_project/soohwan/SoohwanApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa_book_project.soohwan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoohwanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
